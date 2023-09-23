7de4484cf23475c1b1546416db9b31cb00d000d6
blichiz <luisbtez@gmail.com>
2023-09-23 19:02:19 -0300

    tratando de arreglar githun no encuentra json

--- js/include-html.js
+++ js/include-html.js
@@ -1,13 +1,12 @@
-fetch("/detalle.json")
+fetch("detalle.json")
 .then((respuesta) => respuesta.json())
 .then((datos) => localStorage.setItem("detalle", JSON.stringify(datos)));
-
 // Agregar evento de clic al botón "Detalle" en cada producto
 const botonesDetalle = document.querySelectorAll(".product-box button");
 botonesDetalle.forEach((boton, index) => {
 boton.addEventListener("click", () => {
   // Al hacer clic, redirige a la página de detalles y pasa el índice del producto como parámetro
-  window.location.href = `detalle.html?product=${index-0}`;
+  window.location.href = `detalle.html?product=${index}`;
 });
 });
 
